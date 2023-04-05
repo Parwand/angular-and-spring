f2e67cb4aac94f3bd8420cdab74832f0070b40fe
Parwand <parwand.alsino@hhu.de>
2023-04-05 16:05:24 +0200

    Added Annotation 'CrossOriging'

--- src/main/java/net/parwand/angularandspring/controller/HelloController.java
+++ src/main/java/net/parwand/angularandspring/controller/HelloController.java
@@ -1,12 +1,14 @@
 package net.parwand.angularandspring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
 @Controller
+@CrossOrigin("http://localhost:4200/")
 public class HelloController {
 
     @GetMapping("api/hello")

586a2054d55cbc5db56b34f03a5b738b59e4f154
Parwand <parwand.alsino@hhu.de>
2023-04-05 16:19:56 +0200

    edited the type of the responyEntity in controller

--- src/main/java/net/parwand/angularandspring/controller/HelloController.java
+++ src/main/java/net/parwand/angularandspring/controller/HelloController.java
@@ -1,5 +1,7 @@
 package net.parwand.angularandspring.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,7 @@ import java.util.Date;
 public class HelloController {
 
     @GetMapping("api/hello")
-    public @ResponseBody String hello() {
-        return "Hello, the time at the server is now: " + new Date();
+    public @ResponseBody ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello, the time at the server is now: " + new Date(), HttpStatus.OK);
     }
 }
